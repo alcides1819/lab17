707bb996ff0698ec6a1f393ac06fdda56e6b6004
alcides1819 <78455793+alcides1819@users.noreply.github.com>
2024-12-04 10:57:09 -0500

    validacion

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function mostrarMensaje(){
     alert("Hola Bienvenidos....");
 }
+// Validación de formulario
+const form = document.getElementById('contactForm');
+form.addEventListener('submit', function(event) {
+ const name = document.getElementById('name').value;
+ const email = document.getElementById('email').value;
+ if (name === '' || email === '') {
+ alert('Por favor, completa todos los campos.');
+ event.preventDefault();
+ } else {
+ alert('¡Formulario enviado correctamente!');
+ }
+});
